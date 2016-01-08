329db56468d7f7b203869365f83a7ba68459355a
quadrowin <quadrowin@gmail.com>
2016-01-08 22:09:21 +0300

    initial commit

---
+++ desktop/src/com/quadrolord/epicbattle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quadrolord.epicbattle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quadrolord.epicbattle.EpicBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EpicBattle(), config);
+	}
+}
---
+++ html/src/com/quadrolord/epicbattle/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.quadrolord.epicbattle.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.quadrolord.epicbattle.EpicBattle;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new EpicBattle();
+        }
+}
