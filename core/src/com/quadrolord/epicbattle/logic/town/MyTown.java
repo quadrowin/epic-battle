e67b2ce07e262aeba1b6f5ca06704768db36c0f3
Ilya Kolesnikov <fatumm@gmail.com>
2016-01-17 23:28:22 +0600

    wip логики города

--- core/src/com/quadrolord/epicbattle/logic/town/MyTown.java
+++ core/src/com/quadrolord/epicbattle/logic/town/MyTown.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.utils.ArrayMap;
 import com.quadrolord.epicbattle.logic.Game;
 import com.quadrolord.epicbattle.logic.town.building.Building;
 import com.quadrolord.epicbattle.logic.town.resource.Resource;
+import com.quadrolord.epicbattle.logic.town.tile.Tile;
 
 /**
  * Created by Quadrowin on 16.01.2016.
@@ -15,6 +16,7 @@ public class MyTown {
 
     private Array<Building> mBuildings = new Array<Building>();
     private ArrayMap<Resource, Integer> mResources = new ArrayMap<Resource, Integer>();
+    private Tile[][] mMap = new Tile[][]{};
 
     public MyTown(Game game) {
         mGame = game;
